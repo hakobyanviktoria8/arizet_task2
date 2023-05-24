8211f99f11778f5ed0211d3888fbe3963719302f
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-05-24 15:13:12 +0400

    Install MUI, deploy the app to Vercel, and publish it on GitHub

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./styles/App.css";
+import { Button } from "@mui/material";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Start
+      </Button>
+    </div>
+  );
 }
 
 export default App;
